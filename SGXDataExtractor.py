18d8fcce630cd2a8fc05546cc4b8aeb4e172eba9
edologgerbird <E0406924@u.nus.edu>
2022-03-09 13:48:06 +0800

    updated scraper

--- SGXDataExtractor.py
+++ SGXDataExtractor.py
@@ -9,12 +9,15 @@ class SGXDataExtractor:
         self.url = "https://api.sgx.com/securities/v1.1?params=nc%2Cn%2Ctype%2Cls%2Cm%2Csc%2Cbl%2Csip%2Cex%2Cej%2Cclo%2Ccr%2Ccur%2Cel%2Cr%2Ci%2Ccc%2Cig%2Clf"
 
     def extract_SGX_json_data(self):
+        print("Extracting SGX Data from API ...")
         self.url_request = self.url_request = Request(
             self.url, headers={'User-Agent': 'Mozilla/5.0'})
         self.json_data = json.loads(urlopen(self.url_request).read())[
             "data"]["prices"]
+        print("SGX Data Successfully Extracted")
 
     def populate_SGX_data(self):
+        print("Populating SGX Data Store ...")
         SGX_data_store = {
             "company_name": [],
             "company_code": [],
@@ -30,10 +33,11 @@ class SGXDataExtractor:
             SGX_data_store["listing_board"].append(company["m"])
 
         self.SGX_data_store = pd.DataFrame(SGX_data_store).dropna()
-        print(self.SGX_data_store)
+        print("SGX Data successfully populated")
 
     def SGX_data_to_csv(self):
         self.SGX_data_store.to_csv("SGX_data.csv", index=False)
+        print("SGX Data successfully saved to CSV")
 
     def get_SGX_data(self):
         self.extract_SGX_json_data()
