ef2ba6b4c4a3a68ebe14caacaefc3f6156029eff
edologgerbird <E0406924@u.nus.edu>
2022-03-09 13:48:06 +0800

    updated scraper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import SGXDataExtractor
 
-
-x = SGXDataExtractor.SGXDataExtractor()
-x.get_SGX_data()
+if __name__ == '__main__':
+    sgx_data_extractor_layer = SGXDataExtractor.SGXDataExtractor()
+    sgx_data_extractor_layer.get_SGX_data()
