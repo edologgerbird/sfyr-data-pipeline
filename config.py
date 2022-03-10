4de85931592206c9fd8dcb2a54c292e9f3fded0f
edologgerbird <E0406924@u.nus.edu>
2022-03-10 22:44:41 +0800

    update config and gitignore

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+'''
+Telegram Scraper API
+Input login detail below.
+'''
+
+teleNumber = None
+api_id = None
+api_hash = None
+
+'''
+SGX Data Scraper API
+'''
+
 sgx_api = "https://api.sgx.com/securities/v1.1?params=nc%2Cn%2Ctype%2Cls%2Cm%2Csc%2Cbl%2Csip%2Cex%2Cej%2Cclo%2Ccr%2Ccur%2Cel%2Cr%2Ci%2Ccc%2Cig%2Clf"
