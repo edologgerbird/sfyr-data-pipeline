c4a83be2e9f9ea6f48bd52f91497f9e940cebb5b
Zhou Wai <wongzhouwai@gmail.com>
2022-03-10 22:00:33 +0800

    Include Test Code

--- bigQueryIngest.py
+++ bigQueryIngest.py
@@ -48,17 +48,19 @@ class gbdAPI:
     return self.table_id
 
 
-df = pandas.DataFrame(
-    {
-        'my_string': ['a', 'b', 'c'],
-        'my_int64': [1, 2, 3],
-        'my_float64': [4.0, 5.0, 6.0],
-        'my_timestamp': [
-            pandas.Timestamp("1998-09-04T16:03:14"),
-            pandas.Timestamp("2010-09-13T12:03:45"),
-            pandas.Timestamp("2015-10-02T16:00:00")
-        ],
-    }
-)
+# Code Test
 
-print(gbdAPI().gbqInjestReplace(df,"test.test02"))
+# df = pandas.DataFrame(
+#     {
+#         'my_string': ['a', 'b', 'c'],
+#         'my_int64': [1, 2, 3],
+#         'my_float64': [4.0, 5.0, 6.0],
+#         'my_timestamp': [
+#             pandas.Timestamp("1998-09-04T16:03:14"),
+#             pandas.Timestamp("2010-09-13T12:03:45"),
+#             pandas.Timestamp("2015-10-02T16:00:00")
+#         ],
+#     }
+# )
+
+# print(gbdAPI().gbqInjestReplace(df,"test.test02"))
