8e9cb92a3b2bdcd67fb9139ddf321a52c8137fa2
edologgerbird <E0406924@u.nus.edu>
2022-03-12 22:13:37 +0800

    handling camelcase

--- test.py
+++ test.py
@@ -5,8 +5,7 @@ start_time = time.time()
 
 ticker_extractor_layer = TickerExtractor.TickerExtractor()
 data_in = pd.read_csv("sbr_articles_stocks.csv")
-data_in_df = data_in["Text"]
-df = ticker_extractor_layer.populate_ticker_occurences(data_in_df)
+df = ticker_extractor_layer.populate_ticker_occurences(data_in["Text"])
 df.to_csv("tickers_found.csv")
 
 print("--- %s seconds ---" % (time.time() - start_time))
