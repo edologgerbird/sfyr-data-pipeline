2125e485a0b9615e9335a5f3d7ce41877fcc791a
edologgerbird <E0406924@u.nus.edu>
2022-03-14 00:34:34 +0800

    updated currency map

--- config.py
+++ config.py
@@ -19,9 +19,9 @@ TickerExtractor
 '''
 
 word_mapper = {"Intl": "Int", "intl": "int",
-               "YZJ Shipbldg SGD": "Yangzijiang Shipbuilding", " SPAC": "",
+               "YZJ Shipbldg SGD": "Yangzijiang Shipbuilding", "YZJ": "Yangzijiang", " SPAC": "",
                "Reit": "REIT", "Singtel": "Singapore Telecommunications",
                "SIA": "Singapore Airlines", "STI": "Straits Time Index",
                "OCBC Bank": "OCBC", "CapitaLandInvest": "CapitaLand Invest",
                "JMH": "Jardine Matheson", "Tianjin ZX": "Tianjin Zhongxin",
-               "Nomura Yen1k": "Nomura", "USD": "", "SGD": "", "US$": "", "SG$": ""}
+               "Nomura Yen1k": "Nomura", " USD": "", " SGD": "", " US$": "", " SG$": ""}
