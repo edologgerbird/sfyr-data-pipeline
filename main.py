b068ad1ee4c048ffb0df792e76b061d466826ce9
yaphuiyi <e0420913@u.nus.edu>
2022-03-18 22:36:05 +0800

    Added tickers mentioned and STI movement

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import pandas as pd
 import asyncio
-import TelegramExtractor    
-import SGXDataExtractor
+# import TelegramExtractor    
+# import SGXDataExtractor
+import SBRExtractor
 
 if __name__ == '__main__':
-    sgx_data_extractor_layer = SGXDataExtractor.SGXDataExtractor()
-    sgx_data_extractor_layer.load_SGX_data_from_source()
+    # sgx_data_extractor_layer = SGXDataExtractor.SGXDataExtractor()
+    # sgx_data_extractor_layer.load_SGX_data_from_source()
 
-    tele_data_extractor_layer = TelegramExtractor.TelegramExtractor()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(tele_data_extractor_layer.extract_telegram_messages())
+    sbr_data_extraction_layer = SBRExtractor.SBRExtractor()
+    sbr_data_extraction_layer.load_SBR_data_from_source()
+
+    # tele_data_extractor_layer = TelegramExtractor.TelegramExtractor()
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(tele_data_extractor_layer.extract_telegram_messages())
 
 
