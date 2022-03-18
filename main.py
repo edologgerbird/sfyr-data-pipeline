dc424e08ac9abebafe9afe5355ead6fd2d46ba1a
yaphuiyi <62334893+yaphuiyi@users.noreply.github.com>
2022-03-18 19:54:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-if __name__ == 'main':
-    sbr_data_extraction_layer = SBRExtractor.SBRExtractor()
-    sbr_data_extraction_layer.load_SBR_data_from_source()
+import pandas as pd
+import asyncio
+import TelegramExtractor    
+import SGXDataExtractor
+
+if __name__ == '__main__':
+    sgx_data_extractor_layer = SGXDataExtractor.SGXDataExtractor()
+    sgx_data_extractor_layer.load_SGX_data_from_source()
+
+    tele_data_extractor_layer = TelegramExtractor.TelegramExtractor()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(tele_data_extractor_layer.extract_telegram_messages())
