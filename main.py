7a383b433fadab79e81d9d372461f2d88cb25c04
Chris Ng <chris.ng@erium.de>
2022-03-26 00:50:00 +0800

    Optimize logic and fix comments

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from data_extract.SGXDataExtractor import SGXDataExtractor
 from data_extract.TelegramExtractor import TelegramExtractor
 
 if __name__ == '__main__':
-    start_time = time.time()
-    sgx_data_extractor_layer = SGXDataExtractor()
-    sgx_data_extractor_layer.load_SGX_data_from_source()
+    # start_time = time.time()
+    # sgx_data_extractor_layer = SGXDataExtractor()
+    # sgx_data_extractor_layer.load_SGX_data_from_source()
 
-    print("--- %s seconds ---" % (time.time() - start_time))
-    start_time = time.time()
+    # print("--- %s seconds ---" % (time.time() - start_time))
+    # start_time = time.time()
 
-    sbr_data_extraction_layer = SBRExtractor()
-    sbr_data_extraction_layer.load_SBR_data_from_source()
+    # sbr_data_extraction_layer = SBRExtractor()
+    # sbr_data_extraction_layer.load_SBR_data_from_source()
 
-    print("--- %s seconds ---" % (time.time() - start_time))
+    # print("--- %s seconds ---" % (time.time() - start_time))
     start_time = time.time()
 
     tele_data_extractor_layer = TelegramExtractor()
@@ -22,8 +22,6 @@ if __name__ == '__main__':
     # tele_data_extractor_layer.extract_telegram_messages()
 
     # Extracts from start date to end date
-    # tele_data_extractor_layer.extract_telegram_messages(start_date="20-02-2022", end_date="01-01-2022")
+    tele_data_extractor_layer.extract_telegram_messages(start_date="01-02-2022", end_date="10-02-2022")
 
-    # Extracts from latest data until end date
-    tele_data_extractor_layer.extract_telegram_messages(end_date="01-03-2022")
     print("--- %s seconds ---" % (time.time() - start_time))
