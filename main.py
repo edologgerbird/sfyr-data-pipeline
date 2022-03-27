41453a9c7e7806375f957c32bbe5b41d90cb5d8f
Zhou Wai <wongzhouwai@gmail.com>
2022-03-27 13:48:35 +0800

    Integrating GBQ into SGXDataExtract

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import time
 from data_extract.SBRExtractor import SBRExtractor
 from data_extract.SGXDataExtractor import SGXDataExtractor
 from data_extract.TelegramExtractor import TelegramExtractor
+from data_load.bigQueryAPI import gbqInjest, gbqQuery
 
 if __name__ == '__main__':
-    # start_time = time.time()
-    # sgx_data_extractor_layer = SGXDataExtractor()
-    # sgx_data_extractor_layer.load_SGX_data_from_source()
+    start_time = time.time()
+    sgx_data_extractor_layer = SGXDataExtractor()
+    sgx_data_extractor_layer.load_SGX_data_from_source()
 
     # print("--- %s seconds ---" % (time.time() - start_time))
     # start_time = time.time()
@@ -15,13 +16,13 @@ if __name__ == '__main__':
     # sbr_data_extraction_layer.load_SBR_data_from_source()
 
     # print("--- %s seconds ---" % (time.time() - start_time))
-    start_time = time.time()
+    # start_time = time.time()
 
-    tele_data_extractor_layer = TelegramExtractor()
+    # tele_data_extractor_layer = TelegramExtractor()
     # Extracts all data
     # tele_data_extractor_layer.extract_telegram_messages()
 
     # Extracts from start date to end date
-    tele_data_extractor_layer.extract_telegram_messages(start_date="01-02-2022", end_date="10-02-2022")
+    # tele_data_extractor_layer.extract_telegram_messages(start_date="01-02-2022", end_date="10-02-2022")
 
     print("--- %s seconds ---" % (time.time() - start_time))
