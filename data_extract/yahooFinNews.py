66e7fda94c5d48ac1050d22016888893c99915c2
Zhou Wai <wongzhouwai@gmail.com>
2022-03-30 23:38:09 +0800

    Yahoo_Fin_News Functionality

---
+++ data_extract/yahooFinNews.py
@@ -0,0 +1,29 @@
+from yahoo_fin import news
+from tqdm import tqdm
+
+import pandas as pd
+from data_load.bigQueryAPI import gbqQuery
+
+class yahooFinNews:
+    def __init__(self):
+        self.datasetTable = "SGX.Tickers"
+
+    def getTickerNews(self, tickers, sgFlag=False):
+        output = []
+        print("Yahoo_fin: Extracting Ticker News")
+        for i in tqdm(range(len(tickers))):
+            ticker = tickers[i]
+            if sgFlag:
+                ticker = ticker + ".SI"
+            tickerNews = news.get_yf_rss(ticker)
+            if tickerNews != []:
+                output.append([ticker, tickerNews])
+        output = pd.DataFrame(output)
+        output.columns = ["Ticker", "News"]
+        return output
+    
+    def getSGXTickerNews(self):
+        gbqQueryOutput = gbqQuery().getDataFields(self.datasetTable,"company_code")
+        sgxTickers = gbqQueryOutput.loc[:,"company_code"].tolist()
+        return self.getTickerNews(sgxTickers, True)
+
