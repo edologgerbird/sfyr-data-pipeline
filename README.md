a415fa6df4bea4dcaf21177171ef4e4d3ae4ce49
airflow <e0406924@u.nus.edu>
2022-04-15 10:12:50 +0000

    Readme.md draft

--- README.md
+++ README.md
@@ -1,59 +1,85 @@
-# is3107_g7
+# SFYR Data Pipeline Implementation
 
-Rootdir
+Our Company, Sfyr (pronounced: Sapphire /sæfaɪə(r)/) aims to provide retail investors with the tools and data required to support their high-frequency retail trading decisions and needs. Through the use of a data warehouse, Apache Airflow and public data sources, Sfyr provides its clients with accurate, consolidated and updated stock data.
 
-└── data_extract
+### _Keywords_
 
-    ├── SBRExtractor.py
+_Data Pipeline, Data Engineering, Data Architecture, Data Warehouse, Scheduler, DAG, Airflow, BigQuery, Firestore, FinBERT, Sentiment, Stocks Analysis, Investment Insights, Web Scraping, Google Cloud Monitoring._
 
-    ├── SGXDataExtractor.py
+## IS3107 Group 7 | Authors:
 
-    ├── TelegramExtractor.py
-    
-    ├── YahooFinNewsExtractor.py
+- Loh Hong Tak Edmund (A0199943H)
+- Ng Ting You (A0201672N)
+- Tan Yi Bing (A0204181U)
+- Wong Zhou Wai (A0201509R)
+- Yap Hui Yi (A0203707M)
 
-└── data_transform
+<p align="right">(<a href="#top">back to top</a>)</p>
 
-    ├── STIMovementExtractor.py
+## Codes and Resources Used
 
-    ├── TickerExtractor.py
+**Python Version:** 3.9.5
+**Built with:** [Microsoft Visual Studio Code](https://code.visualstudio.com/),
+[Oracle VM Virtual Box](https://www.virtualbox.org/), [Git](https://git-scm.com/)
+**Notable Packages:** apache-airflow, beautifulsoup4, datetime, firebase-admin, json, numpy, pandas, pandas-gbq, telethon, pandas, parse, pendulum, regex, tokenizers, torch, transformers, virtualenv, yahoo-fin, yfinance (view requirements.txt for full list)
 
-└── data_load
+<p align="right">(<a href="#top">back to top</a>)</p>
 
-    ├── bigQueryAPI.py
-    
-    ├── firestoreAPI.py
-    
-└── data_pipeline
+## Getting Started
 
-    ├── FirestorePipeline.py
-    
-    ├── HeatListPipeline.py
-    
-    ├── yahooFinNewsPipeline.py
-    
-└── data_processing
+### Prerequisites
 
-    ├── FinBertAPI.py
-    
-    ├── HeatListGenerator.py
-    
-└── data_transform
+Make sure you have installed all of the following on your development machine:
 
-    ├── STIMovementExtractor.py
-    
-    ├── TickerExtractor.py
+- Python 3.9.5
+- Oracle VM Virtual Box (Highly recommended for running Airflow)
 
-└── utils
+### Installation
 
-    ├── serviceAccount.json
+We recommend setting up a virtual machine and virtual environment to run this project.
 
-└── csv_store
+### _1. Virtual Machine_
 
-    ├── *.csv    
+To set up a VM Virtual Box, please follow the steps detailed here.
 
-main.py
+The VM image file is located here.
 
-sampleDAG.py
+### _2. Virtual Python Environment_
 
-requirements.txt
+shell commnands
+
+```sh
+pip install virtualenv
+virtualenv <your_env_name>
+source <your_env_name>/bin/active
+```
+
+The requirements.txt file contains Python libraries that your notebooks depend on, and they will be installed using:
+
+```sh
+`pip install -r requirements.txt`
+```
+
+### _3. Setting Up Airflow_
+
+Edit the airflow.cfg config file with the following rules:
+
+```python
+dags_folder = /home/airflow/is3107_g7
+
+enable_xcom_pickling = True
+
+load_examples = False
+```
+
+### _4. Setting Up Databases Access_
+
+Place the Google Authentication Crediential JSON files in `utils/` .
+
+Update `utils/serviceAccount.json` with the name of the credential file
+
+###
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Usage
--- data_extract/yfinanceExtractor.py
+++ data_extract/yfinanceExtractor.py
@@ -504,110 +504,110 @@ class yfinanceExtractor:
 
     def yfinanceQuery(self):
         failed = []
-        try:
-            print("Query Historical Data")
-            self.getHistoricalData()
-            print("Historical Data Query Complete")
-        except:
-            failed.append("Historical Data")
+        # try:
+        #     print("Query Historical Data")
+        #     self.getHistoricalData()
+        #     print("Historical Data Query Complete")
+        # except:
+        #     failed.append("Historical Data")
 
-        try:
-            print("Query Financial Statement")
-            self.getFinancialStatement()
-            print("Financial Statement Query Complete")
-        except:
-            failed.append("Financial Statement")
+        # try:
+        #     print("Query Financial Statement")
+        #     self.getFinancialStatement()
+        #     print("Financial Statement Query Complete")
+        # except:
+        #     failed.append("Financial Statement")
 
-        try:
-            print(">> ========== START: Quarterly Financial Statement Query")
-            print(self.getQuarterlyFinancialStatement())
-            print(">> ========== COMPLETE: Quarterly Financial Statement Query")
-        except:
-            failed.append("Quaterly Financial Statement")
+        # try:
+        #     print(">> ========== START: Quarterly Financial Statement Query")
+        #     print(self.getQuarterlyFinancialStatement())
+        #     print(">> ========== COMPLETE: Quarterly Financial Statement Query")
+        # except:
+        #     failed.append("Quaterly Financial Statement")
 
-        try:
-            print(">> ========== START: ISIN Code Query")
-            self.getISINcode()
-            print(">> ========== COMPLETE: ISIN Code Query")
-        except:
-            failed.append("ISIN Code")
+        # try:
+        #     print(">> ========== START: ISIN Code Query")
+        #     self.getISINcode()
+        #     print(">> ========== COMPLETE: ISIN Code Query")
+        # except:
+        #     failed.append("ISIN Code")
 
-        try:
-            print("Query Earnings and Revenue")
-            self.getEarningsandRevenue()
-            print("Earnings and Revenue Query Complete")
-        except:
-            failed.append("Earnings and Revenue")
+        # try:
+        #     print("Query Earnings and Revenue")
+        #     self.getEarningsandRevenue()
+        #     print("Earnings and Revenue Query Complete")
+        # except:
+        #     failed.append("Earnings and Revenue")
 
-        try:
-            print("Query Quarterly Earnings and Revenue")
-            self.getQuarterlyEarningsandRevenue()
-            print("Quarterly Earnings and Revenue Query Complete")
-        except:
-            failed.append("Quarterly Earnings and Revenue")
+        # try:
+        #     print("Query Quarterly Earnings and Revenue")
+        #     self.getQuarterlyEarningsandRevenue()
+        #     print("Quarterly Earnings and Revenue Query Complete")
+        # except:
+        #     failed.append("Quarterly Earnings and Revenue")
 
-        try:
-            print(">> ========== START: Major Holders Query")
-            self.getMajorHolders()
-            print(">> ========== COMPLETE: Major Holders Query")
-        except:
-            failed.append("Major Holders")
+        # try:
+        print(">> ========== START: Major Holders Query")
+        self.getMajorHolders()
+        print(">> ========== COMPLETE: Major Holders Query")
+        # except:
+        #     failed.append("Major Holders")
 
-        try:
-            print(">> ========== START: Basic Shares Query")
-            self.getBasicShares()
-            print(">> ========== COMPLETE: Basic Shares Query")
-        except:
-            failed.append("Basic Shares")
+        # try:
+        #     print(">> ========== START: Basic Shares Query")
+        #     self.getBasicShares()
+        #     print(">> ========== COMPLETE: Basic Shares Query")
+        # except:
+        #     failed.append("Basic Shares")
 
-        try:
-            print("Query Stock Info")
-            self.getStockInfo()
-            print("Stock Info Query Complete")
-        except:
-            failed.append("Stock Info")
+        # try:
+        #     print("Query Stock Info")
+        #     self.getStockInfo()
+        #     print("Stock Info Query Complete")
+        # except:
+        #     failed.append("Stock Info")
 
-        try:
-            print(">> ========== START: Stock Industry Extraction")
-            self.getStockIndustry()
-            print(">> ========== COMPLETE: Stock Industry Extraction")
-        except:
-            failed.append("Stock industry")
+        # try:
+        #     print(">> ========== START: Stock Industry Extraction")
+        #     self.getStockIndustry()
+        #     print(">> ========== COMPLETE: Stock Industry Extraction")
+        # except:
+        #     failed.append("Stock industry")
 
-        try:
-            print(">> ========== START: Stock Calendar Query")
-            self.getCalendar()
-            print(">> ========== COMPLETE: Stock Calendar Query")
-        except:
-            failed.append("Calendar Query")
+        # try:
+        #     print(">> ========== START: Stock Calendar Query")
+        #     self.getCalendar()
+        #     print(">> ========== COMPLETE: Stock Calendar Query")
+        # except:
+        #     failed.append("Calendar Query")
 
-        try:
-            print(">> ========== START: Analyst Recommendations Query")
-            self.getRecommendations()
-            print(">> ========== COMPLETE: Analyst Recommendations Query")
-        except:
-            failed.append("Analyst Recommendations")
+        # try:
+        #     print(">> ========== START: Analyst Recommendations Query")
+        #     self.getRecommendations()
+        #     print(">> ========== COMPLETE: Analyst Recommendations Query")
+        # except:
+        #     failed.append("Analyst Recommendations")
 
-        try:
-            print(">> ========== START: Stock Analysis Query")
-            self.getAnalysis()
-            print(">> ========== COMPLETE: Stock Analysis Query")
-        except:
-            failed.append("Stock Analysis")
+        # try:
+        #     print(">> ========== START: Stock Analysis Query")
+        #     self.getAnalysis()
+        #     print(">> ========== COMPLETE: Stock Analysis Query")
+        # except:
+        #     failed.append("Stock Analysis")
 
-        try:
-            print(">> ========== START: Mutual Fund Holders Query")
-            self.getMutualFundHolders()
-            print(">> ========== COMPLETE: Mutual Fund Holders Query")
-        except:
-            failed.append("Mutual Fund Holder")
+        # try:
+        #     print(">> ========== START: Mutual Fund Holders Query")
+        #     self.getMutualFundHolders()
+        #     print(">> ========== COMPLETE: Mutual Fund Holders Query")
+        # except:
+        #     failed.append("Mutual Fund Holder")
 
-        try:
-            print(">> ========== START: Institutional Holders Querys")
-            self.getInstitutionalHolders()
-            print(">> ========== COMPLETE: Institutional Holders Query")
-        except:
-            failed.append("Institutional Holders")
+        # try:
+        #     print(">> ========== START: Institutional Holders Querys")
+        #     self.getInstitutionalHolders()
+        #     print(">> ========== COMPLETE: Institutional Holders Query")
+        # except:
+        #     failed.append("Institutional Holders")
 
         for name, df in self.yfinanceData.items():
             df.to_csv(f"output_store/{name}.csv", index=False)
