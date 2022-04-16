e134f1b520589e0e826894aabf46b2b28b0888ec
airflow <e0406924@u.nus.edu>
2022-04-16 02:32:24 +0000

    updated main_DAG

--- data_extract/yfinanceExtractor.py
+++ data_extract/yfinanceExtractor.py
@@ -582,11 +582,11 @@ class yfinanceExtractor:
         except:
             failed.append("Institutional Holders")
 
-        # for name, df in self.yfinanceData.items():
-        #     df.to_csv(f"output_store/{name}.csv", index=False)
+        for name, df in self.yfinanceData.items():
+            df.to_csv(f"output_store/{name}.csv", index=False)
 
-        # failed_df = pd.DataFrame(failed)
-        # failed_df.to_csv("output_store/failed.csv", index=False)
+        failed_df = pd.DataFrame(failed)
+        failed_df.to_csv("output_store/failed.csv", index=False)
 
         for failure in failed:
             print(failure)
